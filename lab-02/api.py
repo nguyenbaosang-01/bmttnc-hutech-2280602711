6e84d915d3179fa10f86295c2bf466b952d8fb04
nguyenbaosang-01 <baosangnguyen09@gmail.com>
2025-05-29 11:33:50 +0700

    buoi3

--- lab-02/api.py
+++ lab-02/api.py
@@ -23,5 +23,5 @@ def caesar_decrypt():
     return jsonify({'decrypted_message': decrypted_text})
 
 # Main function
-if __name__ == "__main__":
+if __name__== "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
